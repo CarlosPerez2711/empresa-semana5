a965e49ee0add2dfd310b02d4afc270c751810b9
CarlosPerez2711 <carlosperez271101@gmail.com>
2024-07-07 17:25:02 -0500

    Update PersonasController.php

--- app/Http/Controllers/PersonasController.php
+++ app/Http/Controllers/PersonasController.php
@@ -9,10 +9,8 @@ class PersonasController extends Controller
 {
     public function index()
     {
-        // Obtener las personas con paginación
-        $personas = Persona::simplepaginate(3); // Cambia el número 10 por la cantidad de registros por página que desees
+        $personas = Persona::simplepaginate(3); 
 
-        // Retornar la vista con los datos de personas
         return view('personas.index', compact('personas'));
     }
 }
